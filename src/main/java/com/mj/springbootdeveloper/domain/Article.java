6762a1d4c0d8b21c2cf7ffbe3e8a0368bdb86cb2
MJ0501 <mjay.do@gmail.com>
2024-10-20 23:51:06 +0900

    글 상세화면êREAD구현

--- src/main/java/com/mj/springbootdeveloper/domain/Article.java
+++ src/main/java/com/mj/springbootdeveloper/domain/Article.java
@@ -5,7 +5,13 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -27,4 +33,11 @@ public class Article {
         this.title= title;
         this.content = content;
     }
+    @CreatedDate //Entity가 생성될 때 생성 시간 저장
+    @Column(name="created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedBy //Entity가 수정될 때 수정 시간 저장
+    @Column(name="updated_at")
+    private LocalDateTime updatedAt;
 }
